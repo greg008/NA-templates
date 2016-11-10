213ed822f4ba5bbcaee50743160386ae1b22fa6a
greg <greg008@poczta.fm>
2016-11-10 16:02:12 +0000

    commit find if, erase, inner product templates

--- 40-algos-impl/vector_erase.h
+++ 40-algos-impl/vector_erase.h
@@ -1,4 +1,14 @@
 #pragma once
 
-// define vector_erase here
+#include <vector>
+#include <algorithm>
+
+template<typename T>
+void vector_erase(T & vec,  const typename T::value_type & value)
+{vec.erase(std::remove(vec.begin(),vec.end(),value),vec.end());
+}
+
+
+
+
 
